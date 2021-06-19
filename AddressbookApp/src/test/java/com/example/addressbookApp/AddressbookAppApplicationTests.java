c8d26858c22a4947f7bb255885c3ed235515707e
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-06-19 12:18:55 +0530

    Creating a Address Book App Spring Project

---
+++ AddressbookApp/src/test/java/com/example/addressbookApp/AddressbookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addressbookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressbookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
